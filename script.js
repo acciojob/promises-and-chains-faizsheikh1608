39fe01b9dc2d2db3d169dac154999188544efd34
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2025-01-07 00:35:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const btn = document.querySelector("#btn");
+ const name = document.querySelector("#name").value.trim();
+  const age = Number(document.querySelector("#age").value.trim());
+
+btn.addEventListener('click',() =>{
+	  if (!name || !age) {
+    alert("Both fields are required!");
+    return;
+  }
+	let promise = new Promise((resolve,reject) => {
+		const age = (age);
+		if(age > 18){
+			resolve(`Welcome ${name}, You can vote.`)
+		}else{
+		reject(`Oh sorry ${name}, You aren't old enough.`)
+		}
+	});
+
+	promise.then((res) =>{
+		alert(res);
+	}).catch((err) => {
+		alert(err)
+	})
+});
